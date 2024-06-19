a7dd4493273f6a37e9015823407f98c1986bcebf
DuilioSeghezzi <contact@duilioseghezzi.it>
2024-06-19 14:03:30 +0200

    fixed footer issue

--- components/footer/index.vue
+++ components/footer/index.vue
@@ -4,7 +4,7 @@
 
 <template>
     <div
-        class="px-5 py-5 bg-Background text-center text-Text dark:bg-BackgroundDark dark:text-TextDark flex flex-row justify-between mx-12">
+        class="py-5 px-5 bg-Background text-center text-Text dark:bg-BackgroundDark dark:text-TextDark flex flex-row justify-between w-full">
         <p>
             All Rights Reserved
         </p>
--- components/navbar/link.vue
+++ components/navbar/link.vue
@@ -10,6 +10,6 @@ console.log(props.text);
 <template>
     
     <li class="text-Text dark:text-TextDark cursor-pointer select-none w-fit h-full px-4 py-5 transition-colors rounded-md hover:bg-Accent dark:hover:bg-AccentDark hover:text-Background dark:hover:text-BackgroundDark duration-200 ease-linear hover:shadow-lg hover:dark:shadow-Secondary hover:shadow-SecondaryDark/50 capitalize" >
-        <a :href="`#${text}`" class="w-fit h-full p-6">{{ text }}</a>
+        <a :href="`/#${text}`" class="w-fit h-full p-6">{{ text }}</a>
     </li>
 </template>
